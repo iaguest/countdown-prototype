63c05d7c9c35507782f392c6b545e49b994f4cd7
ianguest <iag.1.apps@gmail.com>
2020-01-21 16:25:20 +0000

    more work on numbers game

--- countdown/main.cpp
+++ countdown/main.cpp
@@ -35,21 +35,21 @@ std::vector<std::string> getSolutionWords(const std::vector<std::string>& words,
 
 void wordGame(const std::string& path)
 {
-    const std::size_t lettersBoardSize = 9;
-    const std::string letters = io::getLetters(path, "letter_distribution.txt");
-    assert (letters.size());
-
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    
+    const std::size_t lettersBoardSize = 9;
+    std::string letters = io::getLetters(path, "letter_distribution.txt");
+    assert (letters.size());
+    
+    std::shuffle(begin(letters), end(letters), gen);
     
     std::vector<std::string> words = io::getWords(path, "UK_english_truncated.txt");
     std::shuffle(begin(words), end(words), gen);
     
-    const int lettersSize = static_cast<int>(letters.size());
-    std::uniform_int_distribution<> dis(0, lettersSize - 1);
-    
     std::string lettersBoard;
-    std::generate_n(std::back_inserter(lettersBoard), lettersBoardSize, [&](){ return letters[dis(gen)]; });
+    std::sample(begin(letters), end(letters), std::back_inserter(lettersBoard),
+                lettersBoardSize, std::mt19937{std::random_device{}()});
     std::cout << lettersBoard << std::endl << std::endl;
     
     std::string guess;
@@ -76,10 +76,26 @@ void wordGame(const std::string& path)
 int main(int argc, const char * argv[])
 {
     const std::string path = "/Users/ianguest/C++/countdown/countdown";
-
-    static const std::array largeNumbers{ 25, 50, 75, 100 };
-    static const std::array smallNumbers{ 1 , 1 , 2 , 2 , 3 , 3 , 4 , 4 , 5 , 5 , 6 , 6 , 7 , 7 , 8 , 8 , 9 , 9 , 10 , 10 };
+    static const int numbersBoardSize = 6;
     
+    std::random_device rd;  // used to seed random number engine
+    std::mt19937 gen(rd()); // random engine
+    std::array largeNumbers{ 25, 50, 75, 100 };
+    std::array smallNumbers{ 1 , 1 , 2 , 2 , 3 , 3 , 4 , 4 , 5 , 5 , 6 , 6 , 7 , 7 , 8 , 8 , 9 , 9 , 10 , 10 };
+    std::shuffle(begin(smallNumbers), end(smallNumbers), gen);
+    
+    std::vector<int> numbersBoard;
+    
+    std::size_t numLargeNumbers = std::uniform_int_distribution<>(0, largeNumbers.size())(gen);
+
+    std::sample(rbegin(largeNumbers), rend(largeNumbers), std::back_inserter(numbersBoard),
+                numLargeNumbers, std::mt19937{std::random_device{}()});
+    std::sample(smallNumbers.begin(), smallNumbers.end(), std::back_inserter(numbersBoard),
+                numbersBoardSize - numLargeNumbers, std::mt19937{std::random_device{}()});
+    
+    for (auto i: numbersBoard) std::cout << i << std::endl;
+    
+    wordGame(path);
     
     return 0;
 }
