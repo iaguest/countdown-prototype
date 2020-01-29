3227cfc50419f63bc321901be284234d0325dc54
ianguest <iag.1.apps@gmail.com>
2020-01-29 21:34:12 +0000

    getPostFixExpression testing

--- countdown_tests/NumbersGameUtilsTests.cpp
+++ countdown_tests/NumbersGameUtilsTests.cpp
@@ -10,20 +10,35 @@
 
 #include "../../catch/catch.hpp"
 
+#include <iterator>
 #include <string>
 #include <vector>
 
 #include "../countdown/NumbersGameUtils.hpp"
 
+using namespace NumbersGameUtils;
 
 TEST_CASE("tokenizeExpression splits simple expression into tokens.")
 {    
     REQUIRE_THAT(std::vector<std::string>({ "(", "9", "+", "99", ")", "*", "(", "1", "-", "2", "/", "32", ")" }), 
-                 Catch::Equals(NumbersGameUtils::tokenizeExpression(std::string("(9+99)*(1-2/32)"))));
+                 Catch::Equals(tokenizeExpression("(9+99)*(1-2/32)")));
 }
 
 TEST_CASE("tokenizeExpression ignores spaces.")
 {
     REQUIRE_THAT(std::vector<std::string>({ "(", "9", "+", "99", ")" }), 
-                 Catch::Equals(NumbersGameUtils::tokenizeExpression(std::string(" ( 9 + 99) "))));
+                 Catch::Equals(tokenizeExpression(" ( 9 + 99) ")));
+}
+
+TEST_CASE("getPostFixExpression converts simple infix expression.")
+{
+    const std::vector<std::string>& result = getPostFixExpression(tokenizeExpression("3+4"));
+    REQUIRE_THAT(std::vector<std::string>({"3","4","+"}), Catch::Equals(result));
+}
+
+TEST_CASE("getPostFixExpression converts more complicated infix expression.")
+{
+    const std::vector<std::string>& result = getPostFixExpression(tokenizeExpression("4+18/(9-3)"));
+    
+    REQUIRE_THAT(std::vector<std::string>({"4","18","9","3","-","/","+"}), Catch::Equals(result));
 }
