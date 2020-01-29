294dfbc2c94212f349549a0f90f40c916449b5cc
ianguest <iag.1.apps@gmail.com>
2020-01-29 11:37:42 +0000

    test stub

---
+++ countdown_tests/Test.cpp
@@ -0,0 +1,15 @@
+//
+//  FirstCatchTest.cpp
+//  countdown_tests
+//
+//  Created by Ian Guest on 29/01/2020.
+//  Copyright © 2020 Ian Guest. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "../../catch/catch.hpp"
+
+TEST_CASE("This is a test!")
+{
+}
