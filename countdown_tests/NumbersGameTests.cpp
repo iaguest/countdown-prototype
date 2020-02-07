cb0168ed44f296410440af82bbff8ca7617a2425
ianguest <iag.1.apps@gmail.com>
2020-02-07 19:58:25 +0000

    extend numbers game tests

--- countdown_tests/NumbersGameTests.cpp
+++ countdown_tests/NumbersGameTests.cpp
@@ -6,11 +6,32 @@
 //  Copyright © 2020 Ian Guest. All rights reserved.
 //
 
+#include <random>
+
 #include "../../catch/catch.hpp"
 
 #include "../countdown/NumbersGame.h"
 
-TEST_CASE("NumbersGame construction succeeds.")
+TEST_CASE("Validate NumbersGame behavior.")
 {
-    REQUIRE_NOTHROW(NumbersGame(1, 1));
+    // initialization
+    std::mt19937 gen(1);
+    
+    SECTION("NumbersGame construction succeeds.")
+    {
+        REQUIRE_NOTHROW(NumbersGame(gen, 1, 1));
+    }
+
+    SECTION("Game board initially empty.")
+    {
+        auto game = NumbersGame(gen, 1, 1);
+        REQUIRE(game.getGameBoard().empty());
+    }
+
+    SECTION("Game board filled on initialize.")
+    {
+        auto game = NumbersGame(gen, 1, 1);
+        game.initialize(); 
+        REQUIRE_THAT("100 75 10 9 4 8", Catch::Equals(game.getGameBoard()));
+    }
 }
