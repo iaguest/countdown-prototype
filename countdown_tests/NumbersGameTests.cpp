b9c28a5f583108575b1eacda4346dd7e1872efba
ianguest <iag.1.apps@gmail.com>
2020-02-08 15:43:05 +0000

    Remove redundant parameter from NumbersGame constructor

--- countdown_tests/NumbersGameTests.cpp
+++ countdown_tests/NumbersGameTests.cpp
@@ -19,31 +19,31 @@ TEST_CASE("Validate NumbersGame behavior.")
     
     SECTION("NumbersGame construction succeeds.")
     {
-        REQUIRE_NOTHROW(NumbersGame(gen, 1, 1));
+        REQUIRE_NOTHROW(NumbersGame(gen, 1));
     }
 
     SECTION("Game board initially empty.")
     {
-        auto game = NumbersGame(gen, 1, 1);
+        auto game = NumbersGame(gen, 1);
         REQUIRE(game.getGameBoard().empty());
     }
 
     SECTION("Game board filled on initialize.")
     {
-        auto game = NumbersGame(gen, 1, 1);
+        auto game = NumbersGame(gen, 1);
         game.initialize(); 
         REQUIRE_THAT("100 75 10 9 4 8", Catch::Equals(game.getGameBoard()));
     }
     
     SECTION("getTarget throws if uninitialized game")
     {
-        auto game = NumbersGame(gen, 1, 1);
+        auto game = NumbersGame(gen, 1);
         REQUIRE_THROWS(game.getTarget());
     }
     
     SECTION("getTarget returns game target value")
     {
-        auto game = NumbersGame(gen, 1, 1);
+        auto game = NumbersGame(gen, 1);
         game.initialize();
         REQUIRE(416 == game.getTarget());
     }
