eefa78886f80580ffcbcdbab36b76ed2369d15dc
ianguest <iag.1.apps@gmail.com>
2020-02-08 16:10:00 +0000

    move file

---
+++ countdown/WordType.h
@@ -0,0 +1,18 @@
+//
+//  WordType.h
+//  countdown
+//
+//  Created by Ian Guest on 08/02/2020.
+//  Copyright © 2020 Ian Guest. All rights reserved.
+//
+
+#ifndef WordType_h
+#define WordType_h
+
+enum class WordType
+{
+    CONSONANT,
+    VOWEL
+};
+
+#endif /* WordType_h */
