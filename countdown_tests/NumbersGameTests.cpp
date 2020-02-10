1171d60fc6a2db5711aa42f75aac80638f62c9cd
ianguest <iag.1.apps@gmail.com>
2020-02-10 19:54:38 +0000

    try use of streams in IGame::Initialize

--- countdown_tests/NumbersGameTests.cpp
+++ countdown_tests/NumbersGameTests.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <random>
+#include <sstream>
 
 #include "../../catch/catch.hpp"
 
@@ -16,35 +17,37 @@ TEST_CASE("Validate NumbersGame behavior.")
 {
     // initialization
     std::mt19937 gen(1);
+    std::ostringstream oss;
+    std::istringstream iss("2");
     
     SECTION("NumbersGame construction succeeds.")
     {
-        REQUIRE_NOTHROW(NumbersGame(gen, 1));
+        REQUIRE_NOTHROW(NumbersGame(gen));
     }
 
     SECTION("Game board initially empty.")
     {
-        auto game = NumbersGame(gen, 1);
+        auto game = NumbersGame(gen);
         REQUIRE(game.getGameBoard().empty());
     }
 
     SECTION("Game board filled on initialize.")
     {
-        auto game = NumbersGame(gen, 1);
-        game.initialize(); 
-        REQUIRE_THAT("100 75 10 9 4 8", Catch::Equals(game.getGameBoard()));
+        auto game = NumbersGame(gen);
+        game.initialize(oss, iss); 
+        REQUIRE_THAT("75 25 10 9 4 8", Catch::Equals(game.getGameBoard()));
     }
     
     SECTION("getTarget throws if uninitialized game")
     {
-        auto game = NumbersGame(gen, 1);
+        auto game = NumbersGame(gen);
         REQUIRE_THROWS(game.getTarget());
     }
     
     SECTION("getTarget returns game target value")
     {
-        auto game = NumbersGame(gen, 1);
-        game.initialize();
+        auto game = NumbersGame(gen);
+        game.initialize(oss, iss);
         REQUIRE(416 == game.getTarget());
     }
 }
