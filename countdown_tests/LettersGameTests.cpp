a370b86a1da851c3af245de082ce7f686eacd68b
ianguest <iag.1.apps@gmail.com>
2020-02-11 09:57:15 +0000

    tidy up

---
+++ countdown_tests/LettersGameTests.cpp
@@ -0,0 +1,20 @@
+//
+//  LettersGameTests.cpp
+//  countdown_tests
+//
+//  Created by Ian Guest on 11/02/2020.
+//  Copyright © 2020 Ian Guest. All rights reserved.
+//
+
+#include "../../catch/catch.hpp"
+
+#include "../countdown/LettersGame.h"
+
+TEST_CASE("Validate LettersGame behavior.")
+{
+    // initialization
+    std::mt19937 gen(1);
+    std::ostringstream oss;
+    std::istringstream iss;
+    
+}
