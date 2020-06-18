667466e16937f115373183308a0cfec02eb0d3df
ianguest <iag.1.apps@gmail.com>
2020-06-18 13:15:27 +0100

    filtered expressions approach was wrong; reduce ops to improve performance

--- countdown/main.cpp
+++ countdown/main.cpp
@@ -47,34 +47,34 @@ void printGenerator(IGenerator<T>& g)
 int main(int argc, const char * argv[])
 {
     Timer t;
-    ExpressionsGenerator foo(std::vector{1,2,3,4});
-//    SubPermutationsGenerator foo(std::vector<int>{1,2,3,4,5,6}, 4);
-//    printGenerator(std::cout, foo);
-    int i = 0;
-    do {
-        foo.next();
-        //simples.push_back(foo.currentItem());
-        ++i;
-    } while(!foo.isDone());    
-    std::cout << i << std::endl;
-    std::cout << "Elapsed time: " << t.elapsed() << std::endl;
+//    ExpressionsGenerator foo(std::vector{1,2,3,4});
+////    SubPermutationsGenerator foo(std::vector<int>{1,2,3,4,5,6}, 4);
+////    printGenerator(std::cout, foo);
+//    int i = 0;
+//    do {
+//        foo.next();
+//        //simples.push_back(foo.currentItem());
+//        ++i;
+//    } while(!foo.isDone());    
+//    std::cout << i << std::endl;
+//    std::cout << "Elapsed time: " << t.elapsed() << std::endl;
     
-//    std::mt19937 gen(std::random_device{}());
-//    
-//    const std::string resourcePath = "/Users/ianguest/C++/countdown/countdown/resources";
-//    
-//    std::vector<char> vowels = Io::getLetters(resourcePath, "vowels.txt");
-//    std::vector<char> consonants = Io::getLetters(resourcePath, "consonants.txt");
-//    std::vector<std::string> words = Io::getWords(resourcePath, "UK_english_truncated.txt");
-//        
-//    std::vector<IGame*> games;
-//    LettersGame g1(gen, vowels, consonants, words);
-//    NumbersGame g2(gen);
-//    ConundrumGame g3(gen, words);
-//    //games.push_back(&g1);
-//    games.push_back(&g2);
-//    //games.push_back(&g3);
-//    GameRunner bob(games);
-//    std::cout << bob.execute();
-//    return 0;
+    std::mt19937 gen(std::random_device{}());
+    
+    const std::string resourcePath = "/Users/ianguest/C++/countdown/countdown/resources";
+    
+    std::vector<char> vowels = Io::getLetters(resourcePath, "vowels.txt");
+    std::vector<char> consonants = Io::getLetters(resourcePath, "consonants.txt");
+    std::vector<std::string> words = Io::getWords(resourcePath, "UK_english_truncated.txt");
+        
+    std::vector<IGame*> games;
+    LettersGame g1(gen, vowels, consonants, words);
+    NumbersGame g2(gen);
+    ConundrumGame g3(gen, words);
+    //games.push_back(&g1);
+    games.push_back(&g2);
+    //games.push_back(&g3);
+    GameRunner bob(games);
+    std::cout << bob.execute();
+    return 0;
 }
